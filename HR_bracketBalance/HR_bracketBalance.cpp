93c12401a81f204238a823dfdab3e2480a23ade5
mitcho85 <mitopete@ucsd.edu>
2016-10-19 12:47:01 -0700

    Initial commit
    Failing a few test cases, need to check bracket balance better.

--- HR_bracketBalance/HR_bracketBalance.cpp
+++ HR_bracketBalance/HR_bracketBalance.cpp
@@ -1,11 +1,63 @@
-// HR_bracketBalance.cpp : Defines the entry point for the console application.
-//
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
 
-#include "stdafx.h"
+using namespace std;
 
-
-int main()
-{
-    return 0;
+bool is_balanced(string expression) {
+	stack<char>symbols;
+	for (int i = 0; i < expression.size(); ++i) {
+		char curBracket = expression[i];
+		//if(curBracket == '\0') return true;
+		if (curBracket == '{') symbols.push(curBracket);
+		else if (curBracket == '(') symbols.push(curBracket);
+		else if (curBracket == '[') symbols.push(curBracket);
+		else if (symbols.size() == 0) return false;
+		else if (curBracket == '}') {
+			if (symbols.top() == '{') symbols.pop();
+			else return false;
+		}
+		else if (curBracket == ')') {
+			if (symbols.top() == '(') symbols.pop();
+			else return false;
+		}
+		else if (curBracket == ']') {
+			if (symbols.top() == '[') symbols.pop();
+			else return false;
+		}
+	}
+	return true;
 }
 
+int main() {
+	int t;
+	cin >> t;
+	for (int a0 = 0; a0 < t; a0++) {
+		string expression;
+		cin >> expression;
+		bool answer = is_balanced(expression);
+		if (answer)
+			cout << "YES\n";
+		else cout << "NO\n";
+	}
+	return 0;
+}
